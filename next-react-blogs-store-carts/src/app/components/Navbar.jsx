966e02a5257eaa76c0e5badb0ebaab79a2c677b3
amirmahdi <amirmahdi.imani78@gmail.com>
2025-06-11 16:13:10 +0330

    Add files via upload

--- next-react-blogs-store-carts/src/app/components/Navbar.jsx
+++ next-react-blogs-store-carts/src/app/components/Navbar.jsx
@@ -1,6 +1,11 @@
+'use client'
+
 import Link from "next/link";
+import { useState } from "react";
 
 export default function Navbar() {
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+
   return (
     <nav className="flex justify-between items-center px-10 py-5 bg-gray-800 border-b border-gray-700 shadow-md">
       <div className="flex space-x-7 items-center text-2xl font-bold tracking-wide text-gray-200 relative">
@@ -8,26 +13,33 @@ export default function Navbar() {
           About
         </Link>
 
-        <div className="group relative">
+        <div
+          className="group relative"
+          onClick={() => setIsDropdownOpen((prev) => !prev)}
+          onMouseEnter={() => setIsDropdownOpen(true)}
+          onMouseLeave={() => setIsDropdownOpen(false)}
+        >
           <span className="cursor-pointer hover:text-white transition-colors duration-300">
             Dashboard
           </span>
 
-          <div className="absolute space-y-2 hidden w-57 font-normal text-sm group-hover:block bg-gray-800 z-50">
-            <Link
-              href="/dashboard/blogManagement"
-              className="block px-3 py-3 text-gray-300 hover:bg-gray-700/70 hover:text-white "
-            >
-              Blog Management
-            </Link>
-            
-            <Link
-              href="/dashboard/cart"
-              className="block px-3 py-3 text-gray-300 hover:bg-gray-700/70 hover:text-white "
-            >
-              Shopping Cart Management
-            </Link>
-          </div>
+          {(isDropdownOpen) && (
+            <div className="absolute space-y-2 w-57 font-normal text-sm bg-gray-800 z-50 border border-gray-700 rounded">
+              <Link
+                href="/dashboard/blogManagement"
+                className="block px-3 py-3 text-gray-300 hover:bg-gray-700/70 hover:text-white"
+              >
+                Blog Management
+              </Link>
+
+              <Link
+                href="/dashboard/cart"
+                className="block px-3 py-3 text-gray-300 hover:bg-gray-700/70 hover:text-white"
+              >
+                Shopping Cart Management
+              </Link>
+            </div>
+          )}
         </div>
       </div>
 
